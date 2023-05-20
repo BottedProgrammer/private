1ac02f42c108023b7ca7fbe8b7200c305a3d97ec
BottedProgrammer <134024311+BottedProgrammer@users.noreply.github.com>
2023-05-20 16:21:40 -0300

    Update ModuleScript.lua

--- ModuleScript.lua
+++ ModuleScript.lua
@@ -1,9 +1,10 @@
 local CoreGui = game:GetService("CoreGui")
 local UIS = game:GetService('UserInputService')
+local HttpService = game:GetService("HttpService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local vu = game:GetService("VirtualUser")
 local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
-local vu = game:GetService("VirtualUser")
-
 local FartSounds = {
     "rbxassetid://1995953620",
     "rbxassetid://4858245620",
@@ -13,35 +14,43 @@ local FartSounds = {
     "rbxassetid://5316091696",
     "rbxassetid://4376814302"
 }
-
 if game.PlaceId ~= 5846387555 then
     local message = Instance.new('Message', workspace)
     message.Text = "Hey, you're in the wrong place. This only works in studio mode."
     return
 end
-
 do
     local OldUI = CoreGui:FindFirstChild("AutoBuildGui")
     if OldUI then
         OldUI:Destroy()
     end
 end
-
 warn('\n\n\n\n\nThanks for using RetroStudio Auto Build by discord.gg/FloofyPlasma! \n\n\nPress the insert key to toggle the ui.')
-
+local RemoteFunctions = ReplicatedStorage.RemoteFunctions
+local RemoteEvents = ReplicatedStorage.RemoteEvents
+local CreateObjectEvent = RemoteFunctions.CreateObject
+local ObjectPropertyChangeRequestEvent = RemoteEvents.ObjectPropertyChangeRequested
+local CheckpointEvent = RemoteEvents.ChangeHistoryInteractionRequested
 local AutoBuildGui, MainFrame, TitleLabel, ModelBox, NameBox, StartButton, FartSound = loadstring(game:HttpGet("https://raw.githubusercontent.com/BottedProgrammer/RetroStudio-Auto-Build-Backup/main/UI.lua"))()()
-
+local Properties = loadstring(game:HttpGet("https://raw.githubusercontent.com/BottedProgrammer/RetroStudio-Auto-Build-Backup/main/Properties.lua"))()
 local CreatedInstances = 0
 
 local function CreateNewInstance(ClassName, Parent)
-    local newInstance = Instance.new(ClassName)
-    newInstance.Parent = Parent
-    CreatedInstances = CreatedInstances + 1
-    return newInstance
+    local Success, Result = pcall(CreateObjectEvent.InvokeServer, CreateObjectEvent, ClassName, Parent)
+    if Success then
+        CreatedInstances = CreatedInstances + 1
+        return Result
+    else
+        warn(Result)
+    end
 end
 
 local function SetInstanceProperty(Object, PropertyName, NewValue)
-    Object[PropertyName] = NewValue
+    ObjectPropertyChangeRequestEvent:FireServer(Object, PropertyName, NewValue)
+end
+
+local function SetCheckpoint()
+    CheckpointEvent:FireServer("AddCheckpoint")
 end
 
 local function ScanModel(Model, ServerParent)
@@ -49,73 +58,53 @@ local function ScanModel(Model, ServerParent)
         ServerParent = CreateNewInstance(Model.ClassName, workspace)
         task.spawn(SetInstanceProperty, ServerParent, "Name", Model.Name)
     end
-    
     for _, Child in ipairs(Model:GetChildren()) do
         local Properties = Properties[Child.ClassName]
-        
         if not Properties then
             continue
         end
-        
         local NewObject = CreateNewInstance(Child.ClassName, ServerParent)
         local IsAnchored = Child:GetAttribute("Anchored")
-        
         if IsAnchored ~= nil then
             Child.Anchored = IsAnchored
         end
-        
         if Child:IsA("BasePart") then
             SetInstanceProperty(NewObject, "FormFactor", "Custom")
         end
-        
         for _, Property in ipairs(Properties) do
             SetInstanceProperty(NewObject, Property, Child[Property])
         end
-        
         if IsAnchored ~= nil then
             Child.Anchored = true
         end
-        
         ScanModel(Child, NewObject)
     end
 end
 
 local function GetAssets(AssetId)
-    local Model = game:GetObjects("rbxassetid://" .. AssetId)
-    
-    if not Model then
-        return
-    end
-    
+    local Model = game:GetObjects("rbxassetid://"..AssetId)
+    if not Model then return end
     Model = Model[1]
-    
     for _, Object in ipairs(Model:GetDescendants()) do
         pcall(function()
             Object:SetAttribute("Anchored", Object.Anchored)
             Object.Anchored = true
         end)
     end
-    
     return Model
 end
 
 local function Start(AssetId, ModelName)
     local Model = GetAssets(AssetId)
-    
-    if not Model then
-        return
-    end
-    
+    if not Model then return end
     Model.Name = ModelName
     local StartTime = tick()
     CreatedInstances = 0
-    
     warn('\n\n\nStarting! This may take a while depending on the size of your model.\n\n\nPlease be patient thanks :3\n\n\n')
-    
+    --SetCheckpoint()
     ScanModel(Model)
-    
-    warn('\n\n\nFinished! Took ' .. tick() - StartTime .. ' seconds to create ' .. tostring(CreatedInstances) .. ' instances.\n\n\n')
-    
+    --SetCheckpoint()
+    warn('\n\n\nFinished! Took ' .. tick() - StartTime .. ' seconds to create '.. tostring(CreatedInstances) .. ' instances.\n\n\n')
     Model:Destroy()
 end
 
@@ -139,4 +128,4 @@ Player.Idled:Connect(function()
     vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 end)
 
-return {}
+Return{}
